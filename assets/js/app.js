0035c3f47d041e612f62b890aa108477e7d914bf
Thomas Bousquet <tom@LAPTOP-BRS8DJ45.localdomain>
2020-01-12 23:13:19 +0100

    Premier commit

---
+++ assets/js/app.js
@@ -0,0 +1,130 @@
+
+
+window.onload = function() {
+
+    const valider = document.querySelector('form button');
+
+        valider.onclick = function() {
+            const inputs = document.querySelectorAll('input');
+            const textarea = document.querySelector('textarea');
+
+            var name = inputs[0].value;
+            var email = inputs[1].value;
+            var message = textarea.value;
+
+            let regexUsername = new RegExp(/^\w{3,}$/);
+            let regexEmail = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/);
+            let regexText = new RegExp(/^[a-zA-Z\d\s,.:éàèù()%=€ïîê!?]{10,500}$/);
+            
+            // Animation de validation
+            if (!regexUsername.test(name)) {
+                inputs[0].parentNode.classList.add('invalid');
+                inputs[0].parentNode.classList.add('shake-top');
+            } else {
+                inputs[0].parentNode.classList.add('valid');
+            }
+            inputs[0].onclick = function() {
+                inputs[0].parentNode.classList.remove('invalid');
+                inputs[0].parentNode.classList.remove('valid');
+            }
+            if (!regexEmail.test(email)) {
+                inputs[1].parentNode.classList.add('invalid');
+                inputs[1].parentNode.classList.add('shake-top');
+            } else {
+                inputs[1].parentNode.classList.add('valid');
+            }
+            inputs[1].onclick = function() {
+                inputs[1].parentNode.classList.remove('invalid');
+                inputs[1].parentNode.classList.remove('valid');
+            }
+            if (!regexText.test(message)) {
+                textarea.classList.add('invalid');
+                textarea.classList.add('shake-top');
+
+            } else {
+                textarea.classList.add('valid');
+            }
+            textarea.onclick = function() {
+                textarea.classList.remove('invalid');
+                textarea.classList.remove('valid');
+            }
+            // SEND
+
+            if (regexUsername.test(name) && regexEmail.test(email) && regexText.test(message)) {
+                console.log(`Bonjour ${name} nous vous répondrons à l'adresse ${email}.\n Votre message est : ${message}`);
+            }
+
+        };
+
+    function AnimateBarres2() {
+        const Bars2 = {
+            'angular' : 60,
+            'react' : 15,
+            'vue' : 10,
+            'handlebars' : 45
+        }
+        var top = this.scrollY;
+        const hauteurEcran = window.innerHeight / 1.5;
+        if (top + 250 > hauteurEcran*2) {
+            Object.keys(Bars2).forEach(function (item) {
+                var barre = document.getElementById(item);
+                const barrePosition = barre.getBoundingClientRect().top;
+                const hauteurEcran = window.innerHeight / 1.5;
+            var bar = new ldBar(`#${item}`,{
+                "stroke": "data:ldbar/res,gradient(0,5,#9df,#9fd,#df9,#fd9)",
+                "stroke-width": 10,
+                "value": 0
+                });
+            bar.set(Bars2[item]);
+            window.removeEventListener('scroll', AnimateBarres2);
+            });
+        }
+        console.log(top + " " + hauteurEcran*2)
+    }
+    function AnimateBarres1() {
+            const Bars1 = {
+                'html' : 90,
+                'css' : 80,
+                'php' : 30,
+                'js' : 45,
+            }            
+                var top = this.scrollY;
+                const hauteurEcran = window.innerHeight / 1.5;
+                if (top + 100 > hauteurEcran) {
+                    Object.keys(Bars1).forEach(function (item) {
+                        var barre = document.getElementById(item);
+                        const barrePosition = barre.getBoundingClientRect().top;
+                        const hauteurEcran = window.innerHeight / 1.5;
+                    var bar = new ldBar(`#${item}`,{
+                        "stroke": "data:ldbar/res,gradient(0,5,#9df,#9fd,#df9,#fd9)",
+                        "stroke-width": 10,
+                        "value": 0
+                        });
+                    bar.set(Bars1[item]);
+                    window.removeEventListener('scroll', AnimateBarres1);
+                    });
+                    window.addEventListener('scroll', AnimateBarres2);
+                }
+                console.log(top + " " + hauteurEcran*2)
+            /*
+            Object.keys(Bars).forEach(function (item) {
+               var barre = document.getElementById(item);
+               const barrePosition = barre.getBoundingClientRect().top;
+               const hauteurEcran = window.innerHeight / 1.5;
+
+               if (barrePosition < hauteurEcran) {
+                    var bar = new ldBar(`#${item}`,{
+                        "stroke": "data:ldbar/res,gradient(0,5,#9df,#9fd,#df9,#fd9)",
+                        "stroke-width": 10,
+                        "value": 0
+                        });
+                    bar.set(Bars[item]);
+                    window.removeEventListener('scroll', AnimateBarres);
+                    setTimeout(AnimateNext, 2000);
+               }
+            });
+            */
+    }
+
+   window.addEventListener('scroll', AnimateBarres1);
+}
